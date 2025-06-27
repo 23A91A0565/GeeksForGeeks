8fbe497428c77be36ca1e49c56fe7f3dc2d687b6
VANKALA DURGA PHANI KUMAR <23a91a0565@aec.edu.in>
2025-06-27 12:02:24 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Java Strings | Set 1/java-strings-set-1.java
@@ -0,0 +1,10 @@
+// User function template for Java
+
+class Solution {
+    static String conRevstr(String S1, String S2) {
+        // code here
+        StringBuilder sb=new StringBuilder(S1+S2);
+        sb.reverse();
+        return sb.toString();
+    }
+}
