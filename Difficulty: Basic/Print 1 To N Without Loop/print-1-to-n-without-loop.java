577c86e3732584286362e0779c450b5091f10acd
VANKALA DURGA PHANI KUMAR <23a91a0565@aec.edu.in>
2025-07-04 19:48:49 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Print 1 To N Without Loop/print-1-to-n-without-loop.java
@@ -0,0 +1,15 @@
+class Solution {
+    public void printNos(int n) {
+        // Code here
+        if(n == 1){
+            System.out.print(n+" ");
+            return;
+        }
+        
+        printNos(n - 1);
+        
+        System.out.print(n+" ");
+        
+        return;
+    }
+}
