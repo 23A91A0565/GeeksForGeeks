44a2b52b136fabcd399b8bf56243b2d8df0bf12e
VANKALA DURGA PHANI KUMAR <23a91a0565@aec.edu.in>
2025-08-04 19:32:06 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Recursively Remove Adjacent Duplicates/recursively-remove-adjacent-duplicates.java
@@ -0,0 +1,30 @@
+// User function Template for Java
+
+class Solution {
+    public String removeUtil(String s) {
+        // code here
+        if (s.length() <= 1) return s;
+
+        StringBuilder sb = new StringBuilder();
+        int i = 0;
+
+        while (i < s.length()) {
+            int j = i + 1;
+            while (j < s.length() && s.charAt(i) == s.charAt(j)) {
+                j++;
+            }
+
+            if (j == i + 1) {
+                sb.append(s.charAt(i));
+            }
+
+            i = j;
+        }
+        String result = sb.toString();
+        if (result.length() == s.length()) {
+            return result;
+        } else {
+            return removeUtil(result);
+        }
+    }
+}
