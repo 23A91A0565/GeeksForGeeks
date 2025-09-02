dfdbe9235c81217e12e06b7a1e8919eeb44d0b8b
VANKALA DURGA PHANI KUMAR <23a91a0565@aec.edu.in>
2025-09-02 19:14:44 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Remove duplicates from a sorted doubly linked list/remove-duplicates-from-a-sorted-doubly-linked-list.java
@@ -0,0 +1,21 @@
+// User function Template for Java
+
+class Solution {
+    Node removeDuplicates(Node head) {
+        // Code Here.
+        Node temp=head;
+        while(temp!=null)
+        {
+            Node nextNode=temp.next;
+            
+            while(nextNode!=null && temp.data == nextNode.data)
+            {
+                nextNode=nextNode.next;
+            }
+            temp.next=nextNode;
+            if(nextNode!=null) nextNode.prev=temp;
+            temp=temp.next;
+        }
+        return head;
+    }
+}
