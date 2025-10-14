29d6efd89ffaa3c71d9e8f0b8f08756c8723f5ca
VANKALA DURGA PHANI KUMAR <23a91a0565@aec.edu.in>
2025-10-14 18:26:51 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Maximum Sum Problem/maximum-sum-problem.java
@@ -0,0 +1,10 @@
+// User function Template for Java
+
+class Solution {
+    public int maxSum(int n) {
+        // code here.
+        if(n==0)
+        return 0;
+        return  Math.max(n,(maxSum(n/2)+maxSum(n/3)+maxSum(n/4)));
+    }
+}
