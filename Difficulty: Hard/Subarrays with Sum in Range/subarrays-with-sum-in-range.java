124a986b534438fff9735505f5d771ee82514189
VANKALA DURGA PHANI KUMAR <23a91a0565@aec.edu.in>
2026-08-05 10:10:40 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Hard/Subarrays with Sum in Range/subarrays-with-sum-in-range.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int countSubarray(int[] arr, int l, int r) {
+        // code here
+        return solve(arr,r)-solve(arr,l-1);
+    }
+    public int solve(int arr[],int k){
+        int sum=0;
+        int i=0,j=0;
+        int ans=0;
+        for(j=0;j<arr.length;j++){
+            sum+=arr[j];
+            while(sum>k){
+                sum-=arr[i];
+                i++;
+            }
+            ans+=j-i+1;
+        }
+        return ans;
+    }
+}
